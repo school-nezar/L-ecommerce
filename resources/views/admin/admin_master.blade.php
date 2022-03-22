f6d134fd7c959d03560161b14aa04828165ecfdf
ctp2017 <31874795+school-nezar@users.noreply.github.com>
2022-03-22 12:44:05 +0300

    الدرس الثالث - تصحيح المسارات الخاصة بالملفات التصميمية

--- resources/views/admin/admin_master.blade.php
+++ resources/views/admin/admin_master.blade.php
@@ -6,23 +6,25 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<!--favicon-->
-	<link rel="icon" href="assets/images/favicon-32x32.png" type="image/png" />
+
+	{{ asset('admin.') }}
+	<link rel="icon" href="{{ asset('admin.') }}assets/images/favicon-32x32.png" type="image/png" />
 	<!--plugins-->
-	<link href="assets/plugins/vectormap/jquery-jvectormap-2.0.2.css" rel="stylesheet"/>
-	<link href="assets/plugins/simplebar/css/simplebar.css" rel="stylesheet" />
-	<link href="assets/plugins/perfect-scrollbar/css/perfect-scrollbar.css" rel="stylesheet" />
-	<link href="assets/plugins/metismenu/css/metisMenu.min.css" rel="stylesheet" />
+	<link href="{{ asset('admin/assets/plugins/vectormap/jquery-jvectormap-2.0.2.css') }}" rel="stylesheet"/>
+	<link href="{{ asset('admin/assets/plugins/simplebar/css/simplebar.css') }}" rel="stylesheet" />
+	<link href="{{ asset('admin/assets/plugins/perfect-scrollbar/css/perfect-scrollbar.css') }}" rel="stylesheet" />
+	<link href="{{ asset('admin/assets/plugins/metismenu/css/metisMenu.min.css') }}" rel="stylesheet" />
 	<!-- loader-->
-	<link href="assets/css/pace.min.css" rel="stylesheet" />
-	<script src="assets/js/pace.min.js"></script>
+	<link href="{{ asset('admin/assets/css/pace.min.css') }}" rel="stylesheet" />
+	<script src="{{ asset('admin/assets/js/pace.min.js') }}"></script>
 	<!-- Bootstrap CSS -->
-	<link href="assets/css/bootstrap.min.css" rel="stylesheet">
-	<link href="assets/css/app.css" rel="stylesheet">
-	<link href="assets/css/icons.css" rel="stylesheet">
+	<link href="{{ asset('admin/assets/css/bootstrap.min.css') }}" rel="stylesheet">
+	<link href="{{ asset('admin/assets/css/app.css') }}" rel="stylesheet">
+	<link href="{{ asset('admin/assets/css/icons.css') }}" rel="stylesheet">
 	<!-- Theme Style CSS -->
-	<link rel="stylesheet" href="assets/css/dark-theme.css" />
-	<link rel="stylesheet" href="assets/css/semi-dark.css" />
-	<link rel="stylesheet" href="assets/css/header-colors.css" />
+	<link rel="stylesheet" href="{{ asset('admin/assets/css/dark-theme.css') }}" />
+	<link rel="stylesheet" href="{{ asset('admin/assets/css/semi-dark.css') }}" />
+	<link rel="stylesheet" href="{{ asset('admin/assets/css/header-colors.css') }}" />
 	<title>Rukada - Responsive Bootstrap 5 Admin Template</title>
 </head>
 
@@ -145,29 +147,30 @@
 
 		</div>
 	</div>
+	
 	<!--end switcher-->
 	<!-- Bootstrap JS -->
-	<script src="assets/js/bootstrap.bundle.min.js"></script>
+	<script src="{{ asset('admin/assets/js/bootstrap.bundle.min.js') }}"></script>
 	<!--plugins-->
-	<script src="assets/js/jquery.min.js"></script>
-	<script src="assets/plugins/simplebar/js/simplebar.min.js"></script>
-	<script src="assets/plugins/metismenu/js/metisMenu.min.js"></script>
-	<script src="assets/plugins/perfect-scrollbar/js/perfect-scrollbar.js"></script>
-	<script src="assets/plugins/chartjs/js/Chart.min.js"></script>
-	<script src="assets/plugins/vectormap/jquery-jvectormap-2.0.2.min.js"></script>
-    <script src="assets/plugins/vectormap/jquery-jvectormap-world-mill-en.js"></script>
-	<script src="assets/plugins/jquery.easy-pie-chart/jquery.easypiechart.min.js"></script>
-	<script src="assets/plugins/sparkline-charts/jquery.sparkline.min.js"></script>
-	<script src="assets/plugins/jquery-knob/excanvas.js"></script>
-	<script src="assets/plugins/jquery-knob/jquery.knob.js"></script>
+	<script src="{{ asset('admin/assets/js/jquery.min.js') }}"></script>
+	<script src="{{ asset('admin/assets/plugins/simplebar/js/simplebar.min.js') }}"></script>
+	<script src="{{ asset('admin/assets/plugins/metismenu/js/metisMenu.min.js') }}"></script>
+	<script src="{{ asset('admin/assets/plugins/perfect-scrollbar/js/perfect-scrollbar.js') }}"></script>
+	<script src="{{ asset('admin/assets/plugins/chartjs/js/Chart.min.js') }}"></script>
+	<script src="{{ asset('admin/assets/plugins/vectormap/jquery-jvectormap-2.0.2.min.js') }}"></script>
+    <script src="{{ asset('admin/assets/plugins/vectormap/jquery-jvectormap-world-mill-en.js') }}"></script>
+	<script src="{{ asset('admin/assets/plugins/jquery.easy-pie-chart/jquery.easypiechart.min.js') }}"></script>
+	<script src="{{ asset('admin/assets/plugins/sparkline-charts/jquery.sparkline.min.js') }}"></script>
+	<script src="{{ asset('admin/assets/plugins/jquery-knob/excanvas.js') }}"></script>
+	<script src="{{ asset('admin/assets/plugins/jquery-knob/jquery.knob.js') }}"></script>
 	  <script>
 		  $(function() {
 			  $(".knob").knob();
 		  });
 	  </script>
-	  <script src="assets/js/index.js"></script>
+	  <script src="{{ asset('admin/assets/js/index.js') }}"></script>
 	<!--app JS-->
-	<script src="assets/js/app.js"></script>
+	<script src="{{ asset('admin/assets/js/app.js') }}"></script>
 </body>
 
 </html>
