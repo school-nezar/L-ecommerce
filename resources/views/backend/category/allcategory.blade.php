ae53030bbecc8df4aece969d5bf796e36e4a6393
ctp2017 <31874795+school-nezar@users.noreply.github.com>
2022-03-28 12:39:43 +0300

    الدرس الرابع - تصميم صفحة عرض الأصناف -2

--- resources/views/backend/category/allcategory.blade.php
+++ resources/views/backend/category/allcategory.blade.php
@@ -6,34 +6,28 @@
 <div class="page-content">
      <!--breadcrumb-->
      <div class="page-breadcrumb d-none d-sm-flex align-items-center mb-3">
-          <div class="breadcrumb-title pe-3">Tables</div>
+          <div class="breadcrumb-title pe-3">Categories</div>
           <div class="ps-3">
                <nav aria-label="breadcrumb">
                     <ol class="breadcrumb mb-0 p-0">
                          <li class="breadcrumb-item"><a href="javascript:;"><i class="bx bx-home-alt"></i></a>
                          </li>
-                         <li class="breadcrumb-item active" aria-current="page">Basic Table</li>
+                         <li class="breadcrumb-item active" aria-current="page">Category table</li>
                     </ol>
                </nav>
           </div>
-          <div class="ms-auto">
-               <div class="btn-group">
-                    <button type="button" class="btn btn-primary">Settings</button>
-                    <button type="button" class="btn btn-primary split-bg-primary dropdown-toggle dropdown-toggle-split" data-bs-toggle="dropdown">	<span class="visually-hidden">Toggle Dropdown</span>
-                    </button>
-                    <div class="dropdown-menu dropdown-menu-right dropdown-menu-lg-end">	<a class="dropdown-item" href="javascript:;">Action</a>
-                         <a class="dropdown-item" href="javascript:;">Another action</a>
-                         <a class="dropdown-item" href="javascript:;">Something else here</a>
-                         <div class="dropdown-divider"></div>	<a class="dropdown-item" href="javascript:;">Separated link</a>
-                    </div>
-               </div>
-          </div>
+         
      </div>
      <!--end breadcrumb-->
      <div class="row">
           <div class="col-xl-9 mx-auto">
           
-               <h6 class="mb-0 text-uppercase">Table head</h6>
+               
+              <div dir="rtl">
+               <a href=""><i title="add" class="fadeIn animated bx bx-message-square-add" style="font-size:25px;color:rgb(55, 206, 18);"></i> 
+               </a>  
+
+          </div>
                <hr/>
                <div class="card">
                     <div class="card-body">
@@ -49,13 +43,21 @@
 
                               <tbody>
 
+                                   @php($i = 1)
                          @foreach ($Category as $row)
 
                                    <tr>
-                                        <th scope="row">1</th>
+                                        <th scope="row">{{$i++}}</th>
                                         <td>{{$row->category_name}}</td>
-                                        <td>{{$row->category_image}}</td>
-                                        <td>@mdo</td>
+                                        <td><div class="d-flex align-items-center">
+                                             <div class="recent-product-img">
+                                                 <img src=" {{ $row->category_image }} " alt="">
+                                             </div>
+
+                                         </div> </td>
+                                        <td><a href=""><i title="edit" class="fadeIn animated bx bx-message-square-edit" style="font-size:25px;color:rgb(47, 26, 165);"></i> </a>
+                                             <a href=""><i title="delete" class="fadeIn animated bx bx-trash" style="font-size:25px;color:rgb(233, 19, 30);"></i> </a>
+                                        </td>
                                    </tr>
  
      
