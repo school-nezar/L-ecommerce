985ea9d066fe752d65bf720f238988398cac5ecf
ctp2017 <31874795+school-nezar@users.noreply.github.com>
2022-03-31 13:40:29 +0300

    الدرس السادس - حفظ تفاصيل المنتج في جدول تفاصيل المنتجات

--- app/Http/Controllers/Admin/ProductListController.php
+++ app/Http/Controllers/Admin/ProductListController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\ProductList;
 use App\Models\Category;
+use App\Models\ProductDetails;
 
 class ProductListController extends Controller
 {
@@ -58,6 +59,57 @@ class ProductListController extends Controller
                 'star'=>'5'
      
             ]);
+
+
+            $file1 = $request->file('image_one');
+            // $file=avatar-1.png
+            $filename1 = hexdec(uniqid()).'.'.$file1->getClientOriginalExtension();
+    
+            //$filename=26.3.2022.png
+    
+            $file1->move(public_path('upload/productdetails'), $filename1);
+    
+            $save_url1 = 'http://127.0.0.1:8000/upload/productdetails/'.$filename1 ; 
+
+
+            $file2 = $request->file('image_two');
+            // $file=avatar-1.png
+            $filename2= hexdec(uniqid()).'.'.$file2->getClientOriginalExtension();
+    
+            //$filename=26.3.2022.png
+    
+            $file2->move(public_path('upload/productdetails'), $filename2);
+    
+            $save_url2 = 'http://127.0.0.1:8000/upload/productdetails/'.$filename2 ; 
+
+            
+            $file3 = $request->file('image_three');
+            $filename3= hexdec(uniqid()).'.'.$file3->getClientOriginalExtension();
+            $file3->move(public_path('upload/productdetails'), $filename3);
+            $save_url3 = 'http://127.0.0.1:8000/upload/productdetails/'.$filename3 ; 
+
+
+            $file4 = $request->file('image_four');
+            $filename4= hexdec(uniqid()).'.'.$file4->getClientOriginalExtension();
+            $file4->move(public_path('upload/productdetails'), $filename4);
+            $save_url4 = 'http://127.0.0.1:8000/upload/productdetails/'.$filename4 ; 
+
+
+
+            ProductDetails::insert([
+                'product_id' => $product_id,
+                'image_one' => $save_url1,
+                'image_two' => $save_url2,
+                'image_three' => $save_url3,
+                'image_four' => $save_url4,
+                'short_description' => $request->short_description,
+                'color' =>  $request->color,
+                'size' =>  $request->size,
+                'long_description' => $request->long_description,
+    
+            ]);
+    
+
 
             $notification = array(
                 'message' => 'Product Inserted Successfully',
