d034a1ca2beb99c4fadef36cd778c46cbd7a1559
ctp2017 <31874795+school-nezar@users.noreply.github.com>
2022-04-03 08:19:47 +0300

    الدرس الخامس - برمجة وظائف عرض طلبات الزبائن حسب حالة الطلب

--- app/Http/Controllers/Admin/ProductCartController.php
+++ app/Http/Controllers/Admin/ProductCartController.php
@@ -4,20 +4,30 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\CartOrder;
 
 class ProductCartController extends Controller
 {
     public function PendingOrder(){
 
+        $orders = CartOrder::where('order_status','Pending')->orderBy('id','DESC')->get();
+        return view('backend.orders.pending_orders',compact('orders'));
+
+
+
     }
 
 
     public function ProcessingOrder(){
-        
+        $orders = CartOrder::where('order_status','Processing')->orderBy('id','DESC')->get();
+        return view('backend.orders.processing_orders',compact('orders'));
+
     }
 
     public function CompleteOrder(){
-        
+        $orders = CartOrder::where('order_status','Complete')->orderBy('id','DESC')->get();
+        return view('backend.orders.complete_orders',compact('orders'));
+
     }
 
 }
