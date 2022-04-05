9c31e3e8b4d5ed0af945b67c8f3fe6660a5e03af
ctp2017 <31874795+school-nezar@users.noreply.github.com>
2022-04-05 13:16:25 +0300

    modal -1 الدرس الرابع - إظهار تفاصيل رسائل الزبائن

--- resources/views/backend/contact/contact_all.blade.php
+++ resources/views/backend/contact/contact_all.blade.php
@@ -34,10 +34,11 @@
                                         <td>{{ $item->email }}</td>
                                         <td>{{ Str::limit($item->message, 25, '..') }}</td>
                                         <td>{{ $item->contact_date }}</td>
-                                        <td>
-                                            <a href="{{ route('message.delete', $item->id) }}" class="btn btn-danger"
-                                                id="delete">Delete </a>
-                                        </td>
+                                    
+
+                                        <td><a class="viewbtn"><i title="show" class="fadeIn animated bx bx-show-alt" style="font-size:25px;color:rgb(47, 26, 165);"></i> </a>
+                                            <a href="{{ route('message.delete', $item->id) }}" id="delete" ><i title="delete"   class="fadeIn animated bx bx-trash" style="font-size:25px;color:rgb(233, 19, 30);"></i> </a>
+                                       </td>
                                     </tr>
                                 @endforeach
                             </tbody>
@@ -47,4 +48,56 @@
             </div>
         </div>
    
+
+        <div class="modal fade " id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog modal-l">
+                <div class="modal-content">
+                   <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">New message</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                   </div>
+                   <div class="modal-body">
+                   
+                      <div class="mb-1">
+                        <label for="recipient-name" class="col-form-label">sender:</label>
+                        <input type="text" class="form-control" id="name" readonly>
+                      </div>
+                      <div class="mb-3">
+                         <label for="recipient-name" class="col-form-label">email:</label>
+                         <input type="text" class="form-control" id="email" readonly>
+                        </div>
+                      <div class="mb-3">
+                        <label for="message-text" class="col-form-label">Message:</label>
+                        <textarea class="form-control" rows="10" id="message" readonly></textarea>
+                      </div>
+                    
+                   </div>
+                   <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                  
+                   </div>
+                 </div>
+            </div>
+           </div>
+
+
+
+          
+
+
+
+          <script>
+            $(document).ready(function(){
+                $('.viewbtn').on('click',function(){
+                   $('#exampleModal').modal('show');
+         
+         
+                      });
+                   });
+         
+            </script>
+
+
+
+
 @endsection
